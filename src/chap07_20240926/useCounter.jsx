260a4dd2329a835edf2fbe78fc721ae61b84f266
Mourn5367 <qwerrrt6@gmail.com>
2024-09-26 15:59:51 +0900

    Custom Hook 작성 (useState)

---
+++ src/chap07_20240926/useCounter.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+function useCounter(initialValue)
+{
+    const {count, setCount} = useState(initialValue);
+
+    const increaseCount = () =>
+    {
+        setCount((count) => count++);
+    }
+    const decreaseCount = () =>
+    {
+        setCount((count) => Math.max(count-1,0));
+    }
+
+    return [count, increaseCount,decreaseCount];
+}
+
+export default useCounter;
