f67cefd85dca45937534fd73a671576c2e9d270d
Mourn5367 <71182691+Mourn5367@users.noreply.github.com>
2024-10-11 23:25:05 +0900

    화살표 함수로 안했음

--- src/chap08_20241010/ConfirmButtonNoBind.jsx
+++ src/chap08_20241010/ConfirmButtonNoBind.jsx
@@ -11,7 +11,7 @@ class ConfirmButtonNoBind extends React.Component
 
     }
 
-    handleConfirm()
+    handleConfirm = () =>
     {
         this.setState(prevState => ({
             isConfirmed: !prevState.isConfirmed
